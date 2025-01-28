812c04971e4911d2b6b06e56fadfce52bebea619
Shahid <msa.ansari28@gmail.com>
2025-01-28 17:42:40 +0530

    handling Nested Routes

---
+++ src/app/blog/first/page.tsx
@@ -0,0 +1,3 @@
+export default function FirstBlogPost(){
+    return <h1 className="text-center">Welcome to First Blog</h1>
+}
---
+++ src/app/blog/second/page.tsx
@@ -0,0 +1,3 @@
+export default function SecondBlogPost(){
+    return <h1 className="text-center">Welcome to Second Blog</h1>
+}
