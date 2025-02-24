41711e9ba3cd24da3d5fc90a1333ce5948b66790
Mohammad Shahid <msa.ansari28@gmail.com>
2025-02-24 21:34:53 +0530

    `Added generateMetadata function and updated ProductDetails function to handle metadata generation Dynamic Metadata Configuration`

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,9 +1,17 @@
-export default async function ProductDetails(
-    {params,
+import { Metadata } from "next";
 
-    }: {
-    params: Promise<{productId: string}>
-})
+type Props = {
+    params: Promise<{productId: string}>;
+};
+
+export const generateMetadata = async ({params}: Props): Promise<Metadata> => {
+    const id = (await params).productId;
+    return{
+        title: `Product ${id}`,
+    }
+}
+
+export default async function ProductDetails({params}: { params: Promise<{productId: string}>})
 {
     const productId = (await params).productId;
     return <h1 >Product Details Page {productId}</h1>
