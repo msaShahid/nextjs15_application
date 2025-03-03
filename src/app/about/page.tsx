ed1cf94575742d2e1b9a692e1c109594491ba8b0
Mohammad Shahid <msa.ansari28@gmail.com>
2025-03-03 22:57:14 +0530

    `Added link to home page in About component`

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,3 +1,11 @@
+import Link from "next/link"
+
 export default function About(){
-    return <h1 >About Page</h1>
+    return (
+        <>
+        <h1 >About Page</h1>
+        <Link href="/" className="text-xl text-blue-700">Back to home page</Link>
+        </>
+    )
+
 }
