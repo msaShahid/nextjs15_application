c660a327b15f77cac149c360d32dbc05c222b49c
Mohammad Shahid <msa.ansari28@gmail.com>
2025-03-07 00:16:01 +0530

    `Added async function and delay to Blog component to loading`

--- src/app/blog/page.tsx
+++ src/app/blog/page.tsx
@@ -1,3 +1,10 @@
-export default function Profile(){
+export default async function Profile(){
+
+    await new Promise((resolve) => {
+        setTimeout(() => {
+            resolve("Delay for loading testing")
+        }, 2000)
+    });
+
     return <h1 >Welcome to Blog</h1>
 }
